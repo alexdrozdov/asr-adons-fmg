068213972ffa2d05e550acc20f0dcb085533329a
alex drozdovs <alex.drozdovs@gmail.com>
2014-09-28 20:30:51 +0400

    ????????? ?????? ?????? ??????? ???????? ????????? ????????

--- wavelets/subwavelet.py
+++ wavelets/subwavelet.py
@@ -14,27 +14,26 @@ class SkeletonToWavelet:
     def handle_roots(self, ticket):
         try:
             wavelet = ticket.find_parent_by_data_id(self.wavelet_src).get_data()
-            wv = self.root_to_subwavelet(wavelet, ticket)
+            try:
+                wv = self.root_to_subwavelet(wavelet, ticket)
+            except:
+                return
             t = ticket.create_ticket("subwavelet", wv)
             self.man.push_ticket(t)
         except:
             print traceback.format_exc()
     def root_to_subwavelet(self, wavelet, root_ticket):
         skeleton_root = root_ticket.get_data()
-        try:
-            mean_width = int(root_ticket.find_ticket_by_sticky("mean-width").get_sticky("mean-width"))
-        except:
-            return
+        mean_width = int(root_ticket.find_ticket_by_sticky("mean-width").get_sticky("mean-width"))
         start_index = skeleton_root.relative_start_offset()-2
         stop_index = start_index+mean_width
         wavelet_data = wavelet.get_wavelet()
-        subwavelet = numpy.zeros((wavelet_data.shape[0], mean_width))
-        for i in range(wavelet_data.shape[0]):
-            offset = skeleton_root.relative_offset_at_row(i)
-            subwavelet[i,:] = wavelet_data[i, offset:offset+mean_width]
+        subwavelet = numpy.zeros((wavelet_data.shape[0], 512))
+        offset = skeleton_root.relative_start_offset()
+        subwavelet[:, 0:mean_width+60] = wavelet_data[:, offset-60:offset+mean_width]
         wv = SignalWavelet(range(mean_width), wavelet.get_scale(), subwavelet)
         return wv
 
 def init_module(manager, gui):
-    return [SkeletonToWavelet(manager, "wavelet" ,"skeleton-offsets")]
+    return [SkeletonToWavelet(manager, "wavelet" ,"skeleton-root-valid")]
 
